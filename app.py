b98c2206212e8239e6821862d6cb5c6046d79442
alfonzo <walteralfonzo@yahoo.com>
2024-03-12 22:52:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-from database import load_assets_from_db
+from database import load_assets_from_db, load_positions_per_asset_from_db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
   assets = load_assets_from_db()
-  return render_template('home.html', assets=assets)
+  return render_template('home.html', assets=assets, load_positions_per_asset_from_db=load_positions_per_asset_from_db)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
